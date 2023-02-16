147d1f25fe26bc3612a76a8c822b9d1a69c21bfb
YOUNKAP NINA Duplex <nina.younkap@ccaa.pro>
2023-02-16 05:06:31 -0800

    correct main function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-/* This program print a sentence followed by new line.*/
-
 #include <stdio.h>
-
+/**
+ *  main -Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
-}	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
