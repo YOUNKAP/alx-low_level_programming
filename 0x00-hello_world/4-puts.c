4dd8b426335671b13fed16c7d12bc4e7fe1b5ffe
YOUNKAP NINA Duplex <nina.younkap@ccaa.pro>
2023-02-16 05:03:55 -0800

    add main function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+/* This program print a sentence followed by new line.*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}	
