607521f6162f1c1f418dd693bf7d094edcb6bbe6
YOUNKAP NINA Duplex <nina.younkap@ccaa.pro>
2023-02-20 00:06:28 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -  Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char character;
+
+	for (character = 'a'; character <= 'z'; character++)
+		putchar(character);
+
+	for (character = 'A'; character <= 'Z'; character++)
+		putchar(character);
+
+	putchar('\n');
+
+	return (0);
+}
