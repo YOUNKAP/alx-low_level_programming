c9165242818756d68eea725b364ce7111d9b7ffd
YOUNKAP NINA Duplex <nina.younkap@ccaa.pro>
2023-02-20 00:42:16 -0800

    digit number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main -> print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int nbr;
+
+for (nbr = 0; nbr < 10; nbr++)
+{
+printf("%d", nbr);
+}
+printf("\n");
+return (0);
+}
