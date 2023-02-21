8e50f6d6070a2faa1b40d4fbf1fc6196a3e43f60
YOUNKAP NINA Duplex <nina.younkap@ccaa.pro>
2023-02-21 07:39:51 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number.
+ * @n: The number in question.
+ *
+ * Return: Value of the last digit.
+ */
+int print_last_digit(int n)
+{
+	int l_digit = n % 10;
+
+	if (l_digit < 0)
+		l_digit *= -1;
+
+	_putchar(l_digit + '0');
+
+	return (l_digit);
+}
