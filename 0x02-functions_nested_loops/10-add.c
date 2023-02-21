7b51215dbdb293a39c4d30261f7d14dd0925b868
YOUNKAP NINA Duplex <nina.younkap@ccaa.pro>
2023-02-21 07:53:23 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int add(int nbr1, int nbr2)
+{
+	return (nbr1 + nbr2);
