620dba3622a3847fb7b5a90a4324ecf57e06d0cb
YOUNKAP NINA Duplex <nina.younkap@ccaa.pro>
2023-02-21 07:03:23 -0800

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,13 @@
 
 /**
  * _islower - Checks if a character is lowercase.
+ * @c: The character to be checked.
  *
  * Return: 1 if character is lowercase, 0 otherwise.
  */
-int _islower(int char)
+int _islower(int c)
 {
-	if (char >= 'a' && char <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
